e07ae5e0c0c188cc51bb124a886e86946b25551c
Timur <zairovne@gmail.com>
2020-05-08 20:50:04 +0500

    добавид jshint

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ let appData = {
     income: [],
     savings: false
 
-}
+};
 
 
     for ( let i = 0; i < 2; i++){
         let a = prompt('Введите обязательную статью расходов в этом месяце'),
             b = prompt('Во сколько обойдется?');
-        if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-            && a != '' && b != '' && a.length < 50){
+        if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
             console.log("Done!");
             appData.expenses[a] = b;
         }
         else{
-
+            i= i - 1;
         }
         
-    };
+    }
+
     appData.moneyPerDay = appData.budget / 30;
     
     
